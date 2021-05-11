389ac10122d3dc6adcaaccc22b28623dd29f6fa1
thekathydaley <thekathydaley@gmail.com>
2021-05-11 09:36:09 -0400

    fifth commit

--- Desktop/PerScholas/Classwork/Mod - 2/KDaley_React_Project/react_project/src/components/Header.jsx
+++ Desktop/PerScholas/Classwork/Mod - 2/KDaley_React_Project/react_project/src/components/Header.jsx
@@ -4,7 +4,8 @@ class Header extends Component {
     render() {
         return (
            
-                <header className="App-header"> Daily Food Log and Calorie Counter </header>
+                <header className="App-header"> <b>Daily Food Log and Calorie Counter 
+                <br></br>Daily Goal: 1600 Calories</b></header>
        
         );
     }
